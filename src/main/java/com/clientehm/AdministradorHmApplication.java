eb18cfc8ce5b2ccd344672c142ca264915b91101
Silvestre Fernandes Fernandes da Silva Neto <silvaneto.cc@gmail.com>
2025-06-01 21:46:18 -0300

    Atualizado

--- src/main/java/com/clientehm/AdministradorHmApplication.java
+++ src/main/java/com/clientehm/AdministradorHmApplication.java
@@ -2,9 +2,6 @@ package com.clientehm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean; // Adicionado
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; // Adicionado
-import org.springframework.security.crypto.password.PasswordEncoder; // Adicionado
 
 @SpringBootApplication
 public class AdministradorHmApplication {
